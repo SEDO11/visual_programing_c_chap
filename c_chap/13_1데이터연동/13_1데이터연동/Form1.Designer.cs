0bfd4e5850c2a6ec80f4e2857feea28fa275aa5c
SEDO11 <do11anm@naver.com>
2021-05-31 17:26:35 +0900

    비주얼 프로그래밍

---
+++ c_chap/13_1데이터연동/13_1데이터연동/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿
+namespace _13_1데이터연동
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dB_성적DataSet = new _13_1데이터연동.DB_성적DataSet();
+            this.dBScoreBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dBScoreTableAdapter = new _13_1데이터연동.DB_성적DataSetTableAdapters.DBScoreTableAdapter();
+            this.tableAdapterManager = new _13_1데이터연동.DB_성적DataSetTableAdapters.TableAdapterManager();
+            this.dBScoreBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+            this.dBScoreBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
+            this.dBScoreDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dB_성적DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dBScoreBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dBScoreBindingNavigator)).BeginInit();
+            this.dBScoreBindingNavigator.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dBScoreDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dB_성적DataSet
+            // 
+            this.dB_성적DataSet.DataSetName = "DB_성적DataSet";
+            this.dB_성적DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dBScoreBindingSource
+            // 
+            this.dBScoreBindingSource.DataMember = "DBScore";
+            this.dBScoreBindingSource.DataSource = this.dB_성적DataSet;
+            // 
+            // dBScoreTableAdapter
+            // 
+            this.dBScoreTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.DBScoreTableAdapter = this.dBScoreTableAdapter;
+            this.tableAdapterManager.UpdateOrder = _13_1데이터연동.DB_성적DataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // dBScoreBindingNavigator
+            // 
+            this.dBScoreBindingNavigator.AddNewItem = this.bindingNavigatorAddNewItem;
+            this.dBScoreBindingNavigator.BindingSource = this.dBScoreBindingSource;
+            this.dBScoreBindingNavigator.CountItem = this.bindingNavigatorCountItem;
+            this.dBScoreBindingNavigator.DeleteItem = this.bindingNavigatorDeleteItem;
+            this.dBScoreBindingNavigator.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.dBScoreBindingNavigator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.bindingNavigatorAddNewItem,
+            this.bindingNavigatorDeleteItem,
+            this.dBScoreBindingNavigatorSaveItem});
+            this.dBScoreBindingNavigator.Location = new System.Drawing.Point(0, 0);
+            this.dBScoreBindingNavigator.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.dBScoreBindingNavigator.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.dBScoreBindingNavigator.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.dBScoreBindingNavigator.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.dBScoreBindingNavigator.Name = "dBScoreBindingNavigator";
+            this.dBScoreBindingNavigator.PositionItem = this.bindingNavigatorPositionItem;
+            this.dBScoreBindingNavigator.Size = new System.Drawing.Size(800, 27);
+            this.dBScoreBindingNavigator.TabIndex = 0;
+            this.dBScoreBindingNavigator.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorMoveFirstItem
+            // 
+            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(29, 22);
+            this.bindingNavigatorMoveFirstItem.Text = "처음으로 이동";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(29, 22);
+            this.bindingNavigatorMovePreviousItem.Text = "이전으로 이동";
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorPositionItem
+            // 
+            this.bindingNavigatorPositionItem.AccessibleName = "위치";
+            this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 27);
+            this.bindingNavigatorPositionItem.Text = "0";
+            this.bindingNavigatorPositionItem.ToolTipText = "현재 위치";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(33, 20);
+            this.bindingNavigatorCountItem.Text = "/{0}";
+            this.bindingNavigatorCountItem.ToolTipText = "전체 항목 수";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorMoveNextItem.Text = "다음으로 이동";
+            // 
+            // bindingNavigatorMoveLastItem
+            // 
+            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorMoveLastItem.Text = "마지막으로 이동";
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorAddNewItem.Text = "새로 추가";
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorDeleteItem.Text = "삭제";
+            // 
+            // dBScoreBindingNavigatorSaveItem
+            // 
+            this.dBScoreBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.dBScoreBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("dBScoreBindingNavigatorSaveItem.Image")));
+            this.dBScoreBindingNavigatorSaveItem.Name = "dBScoreBindingNavigatorSaveItem";
+            this.dBScoreBindingNavigatorSaveItem.Size = new System.Drawing.Size(23, 23);
+            this.dBScoreBindingNavigatorSaveItem.Text = "데이터 저장";
+            this.dBScoreBindingNavigatorSaveItem.Click += new System.EventHandler(this.dBScoreBindingNavigatorSaveItem_Click);
+            // 
+            // dBScoreDataGridView
+            // 
+            this.dBScoreDataGridView.AutoGenerateColumns = false;
+            this.dBScoreDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dBScoreDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3});
+            this.dBScoreDataGridView.DataSource = this.dBScoreBindingSource;
+            this.dBScoreDataGridView.Location = new System.Drawing.Point(49, 81);
+            this.dBScoreDataGridView.Name = "dBScoreDataGridView";
+            this.dBScoreDataGridView.RowHeadersWidth = 51;
+            this.dBScoreDataGridView.RowTemplate.Height = 27;
+            this.dBScoreDataGridView.Size = new System.Drawing.Size(300, 220);
+            this.dBScoreDataGridView.TabIndex = 1;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "Number(일련번호)";
+            this.dataGridViewTextBoxColumn1.HeaderText = "Number(일련번호)";
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            this.dataGridViewTextBoxColumn1.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "Name(이름)";
+            this.dataGridViewTextBoxColumn2.HeaderText = "Name(이름)";
+            this.dataGridViewTextBoxColumn2.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "Score(성적)";
+            this.dataGridViewTextBoxColumn3.HeaderText = "Score(성적)";
+            this.dataGridViewTextBoxColumn3.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dBScoreDataGridView);
+            this.Controls.Add(this.dBScoreBindingNavigator);
+            this.Name = "Form1";
+            this.Text = "DB 연동";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dB_성적DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dBScoreBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dBScoreBindingNavigator)).EndInit();
+            this.dBScoreBindingNavigator.ResumeLayout(false);
+            this.dBScoreBindingNavigator.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dBScoreDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DB_성적DataSet dB_성적DataSet;
+        private System.Windows.Forms.BindingSource dBScoreBindingSource;
+        private DB_성적DataSetTableAdapters.DBScoreTableAdapter dBScoreTableAdapter;
+        private DB_성적DataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.BindingNavigator dBScoreBindingNavigator;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.ToolStripButton dBScoreBindingNavigatorSaveItem;
+        private System.Windows.Forms.DataGridView dBScoreDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+    }
+}
+
